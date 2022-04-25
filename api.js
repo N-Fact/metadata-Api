9b0f50fa7c19a0b8d8a1d20c188a90c0b7b6ab8b
tekin7 <tekin_7_@hotmail.com>
2022-04-25 17:26:00 +0300

    update

--- api.js
+++ api.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const fs = require('fs')
 const { getTotalSupply } = require('./contract')
-const baseJsonPath = "./json/"
+const baseUrl = 'https://dwarfknights.s3.eu-central-1.amazonaws.com/nft/';
+const request = require('request');
 const baseImagePath = "/images/"
+
 const app = express();
 
 const port = 3000;
@@ -13,20 +16,16 @@ app.get('/:id', async  (req, res) => {
     })
 
     const id = req.params.id
-    const jsonPath = baseJsonPath+id+".json"
+    const jsonPath = baseUrl+id+".json"
     var supply = await getTotalSupply()
 
     if (parseInt(id) <= parseInt(supply)) {
         try {
             if (req.params.id != 'favicon.ico') {
-                fs.readFile(jsonPath, "utf8", (err, jsonString) => {
-                    if (err) {
-                        console.log("File read failed:", err);
-                        return;
-                    }
-                    var jsonf = JSON.parse(jsonString)
-                    res.json(jsonf);
-                });
+                fetch(jsonPath)
+                    .then(response => response.json())
+                    .then(json => res.json(json))
+                    .catch(err =>  res.status(404).send('Not found'));
             }
         } catch (error) {
             res.status(404).send('Not found');
@@ -38,18 +37,21 @@ app.get('/:id', async  (req, res) => {
 });
 
 app.get('/image/:id', async(req, res) => {
-    // res.set({
-    //     'Content-Type': 'image/png'
-    // })
 
     const id = req.params.id
-    const filepath = baseImagePath+id+".png"
+    const filepath = baseUrl+id+".png"
+    const imgpath =__dirname+ baseImagePath+id+".png"
     var supply = await getTotalSupply()
     
     if (parseInt(id) <= parseInt(supply)) {
         try {
             if (req.params.id != 'favicon.ico') {
-                res.sendFile(__dirname+ filepath);
+                download(filepath, imgpath, function(){
+                    res.sendFile(imgpath)
+                    fs.unlink(imgpath, function (err) {
+                        console.log('File deleted!');
+                    });
+                  });
             }
         } catch (error) {
             res.status(404).send('Not found');
@@ -60,5 +62,13 @@ app.get('/image/:id', async(req, res) => {
     }
 });
 
+var download = function(uri, filename, callback){
+    request.head(uri, function(err, res, body){
+      console.log('content-type:', res.headers['content-type']);
+      console.log('content-length:', res.headers['content-length']);
+  
+      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
+  };
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
