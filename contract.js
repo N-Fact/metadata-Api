ef69b5c04b26e8105a2a7c0f2ac29c154df71da8
Osman Tuzcu <osmantuzcu@gmail.com>
2022-04-25 16:17:02 +0300

    yol

--- contract.js
+++ contract.js
@@ -1,7 +1,7 @@
 const CONTRACT_ADDRESS = "0x3434D450c83Ac2cD1EeEd89019874a134dD09416"
 const API_URL="https://speedy-nodes-nyc.moralis.io/7b2cdb644152ec2b77693bc4/avalanche/testnet"
 const Web3 = require('web3')
-const contract = require("../metaDataApi/artifacts/contracts/testd.sol/testdf.json");
+const contract = require("./artifacts/contracts/testd.sol/testdf.json");
 
 const provider = new Web3.providers.HttpProvider(API_URL);
 const web3 = new Web3(provider);
