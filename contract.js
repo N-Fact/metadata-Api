f6e19a034ad4446d62f6508f7a2e4e2005f924c8
tekin7 <tekin_7_@hotmail.com>
2022-04-26 01:14:43 +0300

    update

--- contract.js
+++ contract.js
@@ -1,7 +1,7 @@
-const CONTRACT_ADDRESS = "0x29267697d2ff7Eff7cD83175a109ADbA7aA7A91f"
+const CONTRACT_ADDRESS = "0xc63d5fBAe773d1d52E98e955cd689c3eaf0113FF"
 const API_URL="https://speedy-nodes-nyc.moralis.io/7b2cdb644152ec2b77693bc4/avalanche/mainnet"
 const Web3 = require('web3')
-const contract = require("./artifacts/contracts/testd.sol/testdf.json");
+const contract = require("./artifacts/contracts/dwarf.sol/dwarfknights.json");
 
 const provider = new Web3.providers.HttpProvider(API_URL);
 const web3 = new Web3(provider);
